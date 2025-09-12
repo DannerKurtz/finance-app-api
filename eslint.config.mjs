b7a4966f8a3ecc51a2367868765f79909dbc18c4
Danner Kurtz <kurtz.danner@gmail.com>
2025-09-11 21:17:17 -0300

    chore: finaly setup in eslint and hooks git

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,7 +1,9 @@
+import js from "@eslint/js";
+import globals from "globals";
 import json from "@eslint/json";
 import { defineConfig } from "eslint/config";
 
 export default defineConfig([
-  { ignores: ["**/*.js", "**/*.cjs", "**/*.mjs"] },
-  { files: ["**/*.json"], plugins: { json }, language: "json/json", extends: ["json/recommended"] },
+  { files: ["**/*.{js,mjs,cjs}"], plugins: { js }, extends: ["js/recommended"], languageOptions: { globals: globals.node } },
+  { files: ["**/*.jsonc"], plugins: { json }, language: "json/jsonc", extends: ["json/recommended"] },
 ]);
---
+++ index.js
@@ -0,0 +1,5 @@
+// index.js
+
+const meuNome = 'FULANO'; // A regra "no-unused-vars" deve pegar essa variável.
+
+console.log('Olá, mundo!', meuNome);
