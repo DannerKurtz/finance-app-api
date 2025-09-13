255ade59af932e5b0aa1f71dfe25369675fa559e
Danner Kurtz <kurtz.danner@gmail.com>
2025-09-13 09:41:10 -0300

    feat: add PostgresCreateUserRepository

--- src/index.js
+++ src/index.js
@@ -1,16 +1,9 @@
 import 'dotenv/config.js';
 import express from 'express';
-import { PostgresHelper } from './database/postgres/helper-postgres.js';
-
 
 const app = express() 
+app.use(express.json())
 
-app.get('/', async (req, res) => {
-  const result = await PostgresHelper.query('SELECT * FROM users;')
-  res.json(result)
-})
-
-
-app.listen(3000, () => {
-  console.log('Server is running on http://localhost:3000')
+app.listen(process.env.PORT, () => {
+  console.log(`Server is running on http://localhost:${process.env.PORT}`);
 })
