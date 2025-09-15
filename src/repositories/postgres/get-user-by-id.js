4aceb61357637d23986d873ec507e63139a7882f
Danner Kurtz <kurtz.danner@gmail.com>
2025-09-15 18:17:17 -0300

    feat: add PostgresGetUserById

---
+++ src/repositories/postgres/get-user-by-id.js
@@ -0,0 +1,12 @@
+import { PostgresHelper } from "../../database/postgres/helper-postgres.js";
+
+export class PostgresGetUserByIdRepository {
+  async execute(userId){
+    const user = await PostgresHelper.query(
+          'SELECT id, first_name, last_name, email, password FROM users WHERE id = $1',
+          [userId]
+        
+        )
+    return user[0];
+  }
+}
