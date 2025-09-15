e45ac6f50f197627b6deb7a72801fc508fb6df67
Danner Kurtz <kurtz.danner@gmail.com>
2025-09-15 18:30:02 -0300

    feat: add route get user by id

--- src/controllers/get-user-by-id.js
+++ src/controllers/get-user-by-id.js
@@ -1,19 +1,21 @@
-import { PostgresGetUserByIdRepository } from "../repositories/postgres/get-user-by-id";
+import { PostgresGetUserByIdRepository } from "../repositories/postgres/get-user-by-id.js";
 import { badRequest, internalServer, ok } from "./helpers.js";
 
 export class GetUserByIdController{
   async execute(httpRequest) {
     try {
-      const userId = httpRequest.params.id;
+      const userId = httpRequest.params.userId;
       
       if(!userId){
         return badRequest({message: 'The user id is required.'});
       }
 
-      const user = await PostgresGetUserByIdRepository.execute(userId);
+      const postgresGetUserByIdRepository = new PostgresGetUserByIdRepository();
+
+      const user = await postgresGetUserByIdRepository.execute(userId);
 
       return ok({user});
-      
+
     } catch (error) {
       console.error(error);
       return internalServer();
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import 'dotenv/config.js';
 import express from 'express';
 import { CreateUserController } from './controllers/create-user.js';
+import { GetUserByIdController } from './controllers/get-user-by-id.js';
 
 const app = express() 
 app.use(express.json())
@@ -11,6 +12,12 @@ app.post('/api/users', async (req, res) => {
   res.status(createUserResponse.statusCode).json(createUserResponse.body);
 })
 
+app.get('/api/users/:userId', async (req, res) => {
+  const getUserByIdController = new GetUserByIdController();
+  const getUserByIdResponse = await getUserByIdController.execute(req);
+  res.status(getUserByIdResponse.statusCode).json(getUserByIdResponse.body);
+});
+  
 
 app.listen(process.env.PORT, () => {
   console.log(`Server is running on http://localhost:${process.env.PORT}`);
