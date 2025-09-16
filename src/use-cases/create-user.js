72ca134347e39f86d1bd66619b3ead863565e023
Danner Kurtz <kurtz.danner@gmail.com>
2025-09-16 18:54:56 -0300

    feat: add verification if e-mail is available

--- src/use-cases/create-user.js
+++ src/use-cases/create-user.js
@@ -1,12 +1,18 @@
 
 import bcrypt from 'bcrypt';
 import { v4 as uuid } from 'uuid';
+import { PostgresGetUserByEmailRepository } from '../repositories/postgres/get-user-by-email.js';
 import { PostgresCreateUserRepository } from './../repositories/postgres/create-user.js';
 
 export class CreateUserUseCase {
   async execute(createUserParams){
     //TODO: verificar se email já existe;
+    const postgresGetUserByEmailRepository = new PostgresGetUserByEmailRepository();
+    const userAlreadyExists = await postgresGetUserByEmailRepository.execute(createUserParams.email);
 
+    if(userAlreadyExists){
+      throw new Error('Email already exists');
+    }
     // gerar Id do usuário;
     const userId = uuid();
 
