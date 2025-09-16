3f1f0f2d28eff5162c89ac05449a3ec7d04dfd59
Danner Kurtz <kurtz.danner@gmail.com>
2025-09-16 19:45:21 -0300

    feat: add PostgresUpdateUserRepository

---
+++ src/repositories/update-user.js
@@ -0,0 +1,25 @@
+import { PostgresHelper } from '../database/postgres/helper-postgres.js';
+
+export class PostgresUpdateUserRepository {
+  async execute(userId, updateUserParams) {
+    const updateFilds = [];
+    const updateValues = [];
+
+    Object.keys(updateUserParams).forEach((key, index) => {
+      updateFilds.push(`${key} = $${index + 1}`);
+      updateValues.push(updateUserParams[key]);
+    });
+    updateValues.push(userId);
+    
+    const updateQuery = `
+      UPDATE users
+      SET ${updateFilds.join(', ')}
+      WHERE id = $${updateValues.length}
+      RETURNING id, name, email, created_at
+    `;
+
+    const updateUserQuery = await PostgresHelper.query(updateQuery, updateValues);
+    return updateUserQuery[0];
+  
+  }
+}
