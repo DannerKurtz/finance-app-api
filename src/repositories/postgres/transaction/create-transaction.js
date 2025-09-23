2a30803f9ae49ba0b3ebb3227a71aa7339924f24
Danner Kurtz <kurtz.danner@gmail.com>
2025-09-22 21:30:32 -0300

    feat: add PostgresCreateTransactionRepository

---
+++ src/repositories/postgres/transaction/create-transaction.js
@@ -0,0 +1,23 @@
+import { PostgresHelper } from "../../../database/postgres/helper-postgres";
+
+export class PostgresCreateTransactionRepository {
+  async execute(createTransactionParams){
+    await PostgresHelper.query(`
+      INSERT INTO transactions (id, user_id, amount, name, date, type) 
+      VALUES ($1, $2, $3, $4, $5, $6)`, 
+      [
+        createTransactionParams.id,
+        createTransactionParams.userId,
+        createTransactionParams.amount,
+        createTransactionParams.name,
+        createTransactionParams.date,
+        createTransactionParams.type
+      ])
+
+       const transactionCreated = await PostgresHelper.query(
+      `SELECT * FROM transactions WHERE id = $1`,
+      [createTransactionParams.id]
+    )
+    return transactionCreated[0];
+  }
+}
