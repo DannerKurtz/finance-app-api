d2dbdf315ea445bb2f813801e1183f1c707e6a42
Danner Kurtz <kurtz.danner@gmail.com>
2025-09-24 19:54:03 -0300

    docs: config eslint and prettier

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,9 +1,10 @@
 import js from "@eslint/js";
-import globals from "globals";
 import json from "@eslint/json";
+import eslintConfigPrettier from "eslint-config-prettier";
 import { defineConfig } from "eslint/config";
+import globals from "globals";
 
 export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs}"], plugins: { js }, extends: ["js/recommended"], languageOptions: { globals: globals.node } },
-  { files: ["**/*.jsonc"], plugins: { json }, language: "json/jsonc", extends: ["json/recommended"] },
+  { files: ["**/*.{js,mjs,cjs}"], plugins: { js }, extends: ["js/recommended", eslintConfigPrettier], languageOptions: { globals: globals.node } },
+  { files: ["**/*.jsonc"], plugins: { json }, language: "json/jsonc", extends: ["json/recommended", eslintConfigPrettier] },
 ]);
