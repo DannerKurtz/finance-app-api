4845bd29bfce25efee4a0efaeeeacf2eff6eddff
Danner Kurtz <kurtz.danner@gmail.com>
2025-09-27 11:09:46 -0300

    fix: adjusted the amount input type and corrected it so that it always has two numbers after the period

--- src/controllers/helpers/transaction.js
+++ src/controllers/helpers/transaction.js
@@ -4,12 +4,16 @@ import { badRequest } from './http.js';
 export const invalidAmountResponse = () => badRequest({message: 'The amount is invalid.'});
 export const invalidTypeResponse  = () => badRequest({message: 'The type is invalid. It must be EARNING, EXPENSE or INVESTMENT.'});
 
-export const checkIfAmountIsValid = (amount) =>(
-  validator.isCurrency(amount.toString(), { 
+export const checkIfAmountIsValid = (amount) =>{
+  
+  if(typeof amount !== 'number'){
+    return false;
+  }
+  return validator.isCurrency(amount.toFixed(2), { 
     digits_after_decimal: [2],
     allow_decimal: true,
     decimal_separator: '.',
     allow_negatives: false
-}));
+})};
 
 export const checkIfTypeIsValid = (type) => (['EARNING', 'EXPENSE', 'INVESTMENT'].includes(type))
