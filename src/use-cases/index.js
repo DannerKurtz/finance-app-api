0f9e4b9b237e9eec414dccd4cec8fe25f1e37d4f
Danner Kurtz <kurtz.danner@gmail.com>
2025-09-27 10:31:00 -0300

    feat: add GetTransactionsByUserIdUseCase

--- src/use-cases/index.js
+++ src/use-cases/index.js
@@ -4,4 +4,5 @@ export * from './user/get-user-by-id.js';
 export * from './user/update-user.js';
 
 export * from './transactions/create-transaction.js';
+export * from './transactions/get-transactions-by-user-id.js';
 
---
+++ src/use-cases/transactions/get-transactions-by-user-id.js
@@ -0,0 +1,17 @@
+import { UserNotFoundError } from "../../errors/user";
+
+export class GetTransactionsByUserIdUseCase {
+  constructor(getTransactionsByUserIdRepository, getUserByIdRepository){
+    this.getTransactionsByUserIdRepository = getTransactionsByUserIdRepository;
+    this.getUserByIdRepository = getUserByIdRepository;
+  }
+  async execute(userId){
+    const user = await this.getUserByIdRepository.execute(userId);
+    if(!user){
+      throw new UserNotFoundError(userId);
+    }
+    const transactions = await this.getTransactionsByUserIdRepository.execute(userId);
+    return transactions;
+  }
+
+}
