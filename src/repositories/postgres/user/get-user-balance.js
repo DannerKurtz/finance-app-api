c9be27f6da03a3f121d12752589b773032149d39
Danner Kurtz <kurtz.danner@gmail.com>
2025-10-04 13:09:13 -0300

    feat: add prisma user balances

--- src/repositories/postgres/user/get-user-balance.js
+++ src/repositories/postgres/user/get-user-balance.js
@@ -1,16 +1,39 @@
-import { PostgresHelper } from "../../../database/postgres/helper-postgres.js";
+import { prisma } from "../../../../prisma/prisma.js";
 
 
 export class PostgresGetUserBalanceRepository{
   async execute(userId){
-    const balance = await PostgresHelper.query( `
-      SELECT * FROM get_user_balance($1)
-      `,
-      [userId]
-    )
+    const {_sum: {amount: totalExpenses}} = await prisma.transaction.aggregate({
+      where: {
+        user_id: userId,
+        type: 'EXPENSE'
+      },
+      _sum: {
+        amount: true
+      }
+    })
+    const {_sum: {amount: totalEarnings}} = await prisma.transaction.aggregate({
+      where: {
+        user_id: userId,
+        type: 'EARNING'
+      },
+      _sum: {
+        amount: true
+      }
+    })
 
-    return {
-      userId,
-      ...balance[0]}
+     const {_sum: {amount: totalInvestments}} = await prisma.transaction.aggregate({
+      where: {
+        user_id: userId,
+        type: 'INVESTMENT'
+      },
+      _sum: {
+        amount: true
+      }
+    })
+    
+    const balance = (totalEarnings ?? 0) - (totalExpenses ?? 0) - (totalInvestments ?? 0)
+
+    return {balance, totalEarnings: totalEarnings ?? 0, totalExpenses: totalExpenses ?? 0, totalInvestments: totalInvestments ?? 0}
   }
 }
