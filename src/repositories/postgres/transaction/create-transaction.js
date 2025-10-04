054df06b36a2fc42f64a9c951fcac0602c0f2caa
Danner Kurtz <kurtz.danner@gmail.com>
2025-10-04 13:37:51 -0300

    feat: prisma create transaction

--- src/repositories/postgres/transaction/create-transaction.js
+++ src/repositories/postgres/transaction/create-transaction.js
@@ -1,24 +1,18 @@
-import { PostgresHelper } from "../../../database/postgres/helper-postgres.js";
+import { prisma } from "../../../../prisma/prisma.js";
 
 export class PostgresCreateTransactionRepository {
   async execute(createTransactionParams){
-    await PostgresHelper.query(`
-      INSERT INTO transactions (id, user_id, amount, name, date, type) 
-      VALUES ($1, $2, $3, $4, $5, $6)`, 
-      [
-        createTransactionParams.id,
-        createTransactionParams.userId,
-        createTransactionParams.amount,
-        createTransactionParams.name,
-        createTransactionParams.date,
-        createTransactionParams.type
-      ])
-
-       const transactionCreated = await PostgresHelper.query(
-      `SELECT * FROM transactions WHERE id = $1`,
-      [createTransactionParams.id]
-    )
+    const { userId, ...transactionData } = createTransactionParams;
     
-    return transactionCreated[0];
+    return await prisma.transaction.create({
+      data: {
+        ...transactionData,
+        user: {
+          connect: {
+            id: userId
+          }
+        }
+      }
+    })
   }
 }
