c4d93a3f267d89d5a0209e54fbd203967de42824
Danner Kurtz <kurtz.danner@gmail.com>
2025-10-04 12:50:26 -0300

    feat add prisma get user by email

--- src/repositories/postgres/user/get-user-by-email.js
+++ src/repositories/postgres/user/get-user-by-email.js
@@ -1,12 +1,12 @@
-import { PostgresHelper } from "../../../database/postgres/helper-postgres.js";
+import { prisma } from "../../../../prisma/prisma.js";
 
 export class PostgresGetUserByEmailRepository {
-  async execute(email){
-    const user = await PostgresHelper.query(
-          'SELECT * FROM users WHERE email = $1',
-          [email]
-        
-        )
-    return user[0];
+  execute(email) {
+    return prisma.user.findUnique({
+      where: {
+        email,
+      },
+    });
   }
+  
 }
