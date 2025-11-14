24413571734ade2554d4c57ecc55f7c963b97af8
Danner Kurtz <kurtz.danner@gmail.com>
2025-11-14 19:31:11 -0300

    chore: configure Jest to ignore postgres_data folder and add Jest globals to ESLint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,10 +1,28 @@
-import js from "@eslint/js";
-import json from "@eslint/json";
-import eslintConfigPrettier from "eslint-config-prettier";
-import { defineConfig } from "eslint/config";
-import globals from "globals";
+import js from '@eslint/js';
+import json from '@eslint/json';
+import eslintConfigPrettier from 'eslint-config-prettier';
+import { defineConfig } from 'eslint/config';
+import globals from 'globals';
 
 export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs}"], plugins: { js }, extends: ["js/recommended", eslintConfigPrettier], languageOptions: { globals: globals.node } },
-  { files: ["**/*.jsonc"], plugins: { json }, language: "json/jsonc", extends: ["json/recommended", eslintConfigPrettier] },
+  {
+    files: ['**/*.{js,mjs,cjs}'],
+    plugins: { js },
+    extends: ['js/recommended', eslintConfigPrettier],
+    languageOptions: { globals: globals.node },
+  },
+  {
+    files: ['**/*.jsonc'],
+    plugins: { json },
+    language: 'json/jsonc',
+    extends: ['json/recommended', eslintConfigPrettier],
+  },
+  {
+    files: ['**/*.test.js', '**/*.spec.js'],
+    languageOptions: {
+      globals: {
+        ...globals.jest,
+      },
+    },
+  },
 ]);
