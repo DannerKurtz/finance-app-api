a55b4691c97ccd44b37f083e33cc672067c3b32f
Danner Kurtz <kurtz.danner@gmail.com>
2026-03-21 16:42:17 -0300

    test: adicionar teste para retorno 500 em caso de erro no GetUserByIdController

--- src/controllers/users/get-user-by-id.test.js
+++ src/controllers/users/get-user-by-id.test.js
@@ -63,4 +63,21 @@ describe('GetUserByIdController', () => {
     //assert
     expect(result.statusCode).toBe(404);
   });
+
+  it('should return 500 is error', async () => {
+    //arrange
+    const { sut, getUserByIdUseCase } = makeSut();
+    jest
+      .spyOn(getUserByIdUseCase, 'execute')
+      .mockRejectedValueOnce(() => new Error());
+
+    //act
+
+    const result = await sut.execute({
+      params: { userId: faker.string.uuid() },
+    });
+
+    //assert
+    expect(result.statusCode).toBe(500);
+  });
 });
