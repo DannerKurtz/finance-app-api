7b2bdf852e170d57a5ad65d709438e6ecd89aa17
Danner Kurtz <kurtz.danner@gmail.com>
2026-03-29 16:01:41 -0300

    refatorar: melhorar a formatação e a legibilidade do código no GetTransactionsByUserIdController

--- src/controllers/transactions/get-transactions-by-user-id.js
+++ src/controllers/transactions/get-transactions-by-user-id.js
@@ -1,29 +1,37 @@
-import { UserNotFoundError } from "../../errors/user.js";
-import { badRequest, checkIfIdIsValid, internalServer, invalidIdResponse, ok, requiredFieldIsMissingResponse } from "../helpers/index.js";
+import { UserNotFoundError } from '../../errors/user.js';
+import {
+  checkIfIdIsValid,
+  internalServer,
+  invalidIdResponse,
+  notFound,
+  ok,
+  requiredFieldIsMissingResponse,
+} from '../helpers/index.js';
 
 export class GetTransactionsByUserIdController {
-  constructor(getTransactionsByUserIdUseCase){
-    this.getTransactionsByUserIdUseCase = getTransactionsByUserIdUseCase
+  constructor(getTransactionsByUserIdUseCase) {
+    this.getTransactionsByUserIdUseCase = getTransactionsByUserIdUseCase;
   }
-  async execute(httpRequest){
-   try {
-     const {userId} = httpRequest.query;
-    if(!userId){
-      return requiredFieldIsMissingResponse('userId');
-    }
-    const checkUserId = checkIfIdIsValid(userId);
-    if(!checkUserId){
-      return invalidIdResponse(userId);
-    }
+  async execute(httpRequest) {
+    try {
+      const { userId } = httpRequest.query;
+      if (!userId) {
+        return requiredFieldIsMissingResponse('userId');
+      }
+      const checkUserId = checkIfIdIsValid(userId);
+      if (!checkUserId) {
+        return invalidIdResponse(userId);
+      }
 
-    const getTransactionsByUserId = await this.getTransactionsByUserIdUseCase.execute(userId);
-    return ok(getTransactionsByUserId); 
-   } catch (error) {
-    console.error(error);
-    if(error instanceof UserNotFoundError){
-      return badRequest({message: error.message});
+      const getTransactionsByUserId =
+        await this.getTransactionsByUserIdUseCase.execute(userId);
+      return ok(getTransactionsByUserId);
+    } catch (error) {
+      console.error(error);
+      if (error instanceof UserNotFoundError) {
+        return notFound({ message: error.message });
+      }
+      return internalServer();
     }
-    return internalServer()
-   }
   }
 }
