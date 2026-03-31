97c4fbcec508d0bc82245e6d83bdc33623d76ae4
Danner Kurtz <kurtz.danner@gmail.com>
2026-03-30 21:52:40 -0300

    refatorar: melhorar a formatação e a legibilidade do GetUserBalanceController

--- src/controllers/users/get-user-balance.js
+++ src/controllers/users/get-user-balance.js
@@ -1,30 +1,34 @@
 import { UserNotFoundError } from '../../errors/user.js';
-import { badRequest, checkIfIdIsValid, internalServer, ok } from '../helpers/index.js';
+import {
+  badRequest,
+  checkIfIdIsValid,
+  internalServer,
+  ok,
+} from '../helpers/index.js';
 
-export class GetUserBalanceController{
-  constructor(getUserBalanceUseCase){
+export class GetUserBalanceController {
+  constructor(getUserBalanceUseCase) {
     this.getUserBalanceUseCase = getUserBalanceUseCase;
   }
-  async execute(httpRequest){
+  async execute(httpRequest) {
     try {
       const userId = httpRequest.params.userId;
 
       const verifyUserId = checkIfIdIsValid(userId);
 
-      if(!verifyUserId){
-        return badRequest({message: 'Invalid user id.'});
+      if (!verifyUserId) {
+        return badRequest({ message: 'Invalid user id.' });
       }
 
-      const balance = await this.getUserBalanceUseCase.execute({userId});
+      const balance = await this.getUserBalanceUseCase.execute(userId);
 
-      return ok({balance});
-
+      return ok({ balance });
     } catch (error) {
-      if(error instanceof UserNotFoundError){
-        return badRequest({message: error.message});
+      if (error instanceof UserNotFoundError) {
+        return badRequest({ message: error.message });
       }
       console.error(error);
-      return internalServer()
+      return internalServer();
     }
   }
 }
