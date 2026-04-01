7f2283b6e61b1fb381504cae5bde079fa431fb39
Danner Kurtz <kurtz.danner@gmail.com>
2026-03-31 21:09:35 -0300

    refatorar: ajustar formatação e melhorar a legibilidade no CreateUserController

--- src/controllers/users/create-user.js
+++ src/controllers/users/create-user.js
@@ -3,34 +3,31 @@ import { EmailAlreadyExistsError } from '../../errors/user.js';
 import { createUserSchema } from '../../schemas/index.js';
 import { badRequest, created, internalServer } from '../helpers/index.js';
 
-export class CreateUserController{
-  constructor(createUserUseCase){
+export class CreateUserController {
+  constructor(createUserUseCase) {
     this.createUserUseCase = createUserUseCase;
-  
   }
   async execute(httpRequest) {
-   try {
-     const params = httpRequest.body;
+    try {
+      const params = httpRequest.body;
 
-    
-    await z.parseAsync(createUserSchema, params);
+      await createUserSchema.parseAsync(params);
 
-    const createdUser = await this.createUserUseCase.execute(params);
+      const createdUser = await this.createUserUseCase.execute(params);
 
-    // retornar a resposta
-    return created({createdUser});
-  
-   } catch (error) {
-    if(error instanceof z.ZodError){
-      return badRequest({message: error.issues[0].message});
+      // retornar a resposta
+      return created({ createdUser });
+    } catch (error) {
+      if (error instanceof z.ZodError) {
+        return badRequest({ message: error.issues[0].message });
+      }
+
+      if (error instanceof EmailAlreadyExistsError) {
+        return badRequest({ message: error.message });
+      }
+
+      console.error(error);
+      return internalServer();
     }
-
-    if(error instanceof EmailAlreadyExistsError){
-      return badRequest({message: error.message});
-    }
-
-    console.error(error);
-    return internalServer();
-    }
-   }
   }
+}
