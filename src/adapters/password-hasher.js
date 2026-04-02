a1e8808f41eeae34db2f68440a36480977e2d690
Danner Kurtz <kurtz.danner@gmail.com>
2026-04-01 21:06:59 -0300

    refatorar: corrigir retorno da função de hash de senha no PasswordHasherAdapter

--- src/adapters/password-hasher.js
+++ src/adapters/password-hasher.js
@@ -1,7 +1,7 @@
-import { bcrypt } from 'bcrypt';
+import bcrypt from 'bcrypt';
 
 export class PasswordHasherAdapter {
   async execute(password) {
-    await bcrypt.hash(password, 10);
+    return await bcrypt.hash(password, 10);
   }
 }
