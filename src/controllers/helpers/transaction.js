b5a32c2f2ad8e102405dcb895f4617df8b6ac570
Danner Kurtz <kurtz.danner@gmail.com>
2026-04-01 21:35:30 -0300

    refatorar: ajustar formatação das mensagens de erro nos helpers de transação e usuário

--- src/controllers/helpers/transaction.js
+++ src/controllers/helpers/transaction.js
@@ -1,19 +1,24 @@
 import validator from 'validator';
 import { badRequest } from './http.js';
 
-export const invalidAmountResponse = () => badRequest({message: 'The amount is invalid.'});
-export const invalidTypeResponse  = () => badRequest({message: 'The type is invalid. It must be EARNING, EXPENSE or INVESTMENT.'});
+export const invalidAmountResponse = () =>
+  badRequest({ message: 'The amount is invalid.' });
+export const invalidTypeResponse = () =>
+  badRequest({
+    message: 'The type is invalid. It must be EARNING, EXPENSE or INVESTMENT.',
+  });
 
-export const checkIfAmountIsValid = (amount) =>{
-  
-  if(typeof amount !== 'number'){
+export const checkIfAmountIsValid = (amount) => {
+  if (typeof amount !== 'number') {
     return false;
   }
-  return validator.isCurrency(amount.toFixed(2), { 
+  return validator.isCurrency(amount.toFixed(2), {
     digits_after_decimal: [2],
     allow_decimal: true,
     decimal_separator: '.',
-    allow_negatives: false
-})};
+    allow_negatives: false,
+  });
+};
 
-export const checkIfTypeIsValid = (type) => (['EARNING', 'EXPENSE', 'INVESTMENT'].includes(type))
+export const checkIfTypeIsValid = (type) =>
+  ['EARNING', 'EXPENSE', 'INVESTMENT'].includes(type);
