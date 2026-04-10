e374d1d35012913d2e4b73c10ee3cd8afdaf9cf5
Danner Kurtz <kurtz.danner@gmail.com>
2026-04-09 21:55:21 -0300

    refatorar: adicionar verificação de erro para e-mail já existente e simplificar criação de usuário nos testes

--- src/use-cases/user/create-user.test.js
+++ src/use-cases/user/create-user.test.js
@@ -1,4 +1,6 @@
 import { faker } from '@faker-js/faker';
+import { jest } from '@jest/globals';
+import { EmailAlreadyExistsError } from '../../errors/user';
 import { CreateUserUseCase } from './create-user';
 
 describe('Create User Use Case', () => {
@@ -43,19 +45,33 @@ describe('Create User Use Case', () => {
       idGeneratorAdapter,
     };
   };
+  const user = {
+    first_name: faker.person.firstName(),
+    last_name: faker.person.lastName(),
+    email: faker.internet.email(),
+    password: faker.internet.password({
+      length: 7,
+    }),
+  };
   it('should successfully create a user', async () => {
     //arrange
     const { sut } = makeSut();
     //act
-    const createdUser = sut.execute({
-      first_name: faker.person.firstName(),
-      last_name: faker.person.lastName(),
-      email: faker.internet.email(),
-      password: faker.internet.password({
-        length: 7,
-      }),
-    });
+    const createdUser = sut.execute(user);
     //assert
     expect(createdUser).toBeTruthy();
+  });
+
+  it('should throws an EmailAlreadyInUseError if GetUserByEmailRepository returns a user', async () => {
+    //arrange
+    const { sut, getUserByEmailRepository } = makeSut();
+    jest.spyOn(getUserByEmailRepository, 'execute').mockReturnValue(user);
+    //act
+
+    const promise = sut.execute(user);
+    //assert
+    await expect(promise).rejects.toThrow(
+      new EmailAlreadyExistsError(user.email),
+    );
   });
 });
