6533dade187aeabaf8b134ed5363296468bf98a2
Danner Kurtz <kurtz.danner@gmail.com>
2026-05-16 20:31:32 -0300

    test: adicionar verificação para lançamento de erro se o usuário não for encontrado

--- src/use-cases/transactions/get-transactions-by-user-id.test.js
+++ src/use-cases/transactions/get-transactions-by-user-id.test.js
@@ -1,4 +1,6 @@
 import { faker } from '@faker-js/faker';
+import { jest } from '@jest/globals';
+import { UserNotFoundError } from '../../errors/user.js';
 import { GetTransactionsByUserIdUseCase } from './get-transactions-by-user-id.js';
 describe('Get Transactions by User ID Use Case', () => {
   const user = {
@@ -45,5 +47,16 @@ describe('Get Transactions by User ID Use Case', () => {
     const result = await sut.execute(userId);
     // Assert
     expect(result).toEqual([]);
+  });
+
+  it('should throw an error if user is not found', async () => {
+    // Arrange
+    const { sut, getUserByIdRepository } = makeSut();
+    const userId = faker.string.uuid();
+    jest.spyOn(getUserByIdRepository, 'execute').mockResolvedValueOnce(null);
+    // Act
+    const promise = sut.execute(userId);
+    // Assert
+    await expect(promise).rejects.toThrow(new UserNotFoundError(userId));
   });
 });
