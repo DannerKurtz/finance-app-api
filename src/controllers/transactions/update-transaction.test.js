dd6c59fd3f05cf1384336f1786b28720a7906710
Danner Kurtz <kurtz.danner@gmail.com>
2026-07-27 19:33:19 -0300

    refactor: substituir dados gerados pelo faker por fixture nos testes do controlador de atualização de transação

--- src/controllers/transactions/update-transaction.test.js
+++ src/controllers/transactions/update-transaction.test.js
@@ -1,17 +1,11 @@
 import { faker } from '@faker-js/faker';
 import { jest } from '@jest/globals';
+import { transaction } from '../../tests';
 import { UpdateTransactionController } from './update-transaction';
 describe('Update Transaction Controller', () => {
   class UpdateTransactionUseCaseStub {
     async execute() {
-      return {
-        id: faker.string.uuid(),
-        userId: faker.string.uuid(),
-        name: faker.commerce.productName(),
-        date: faker.date.anytime().toISOString(),
-        type: 'EXPENSE',
-        amount: Number(faker.finance.amount()),
-      };
+      return transaction;
     }
   }
 
