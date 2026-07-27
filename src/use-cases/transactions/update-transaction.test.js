9d3249344f6dd4a26ca74fbb1cadb72ee10cb781
Danner Kurtz <kurtz.danner@gmail.com>
2026-07-27 19:15:41 -0300

    refactor: remover dependência do faker e utilizar fixture de transação nos testes de atualização

--- src/use-cases/transactions/update-transaction.test.js
+++ src/use-cases/transactions/update-transaction.test.js
@@ -1,23 +1,11 @@
-import { faker } from '@faker-js/faker';
 import { jest } from '@jest/globals';
+import { transaction } from '../../tests';
 import { UpdateTransactionUseCase } from './update-transaction';
 
 describe('Update Transaction Use Case', () => {
-  const transaction = {
-    id: faker.string.uuid(),
-    userId: faker.string.uuid(),
-    name: faker.commerce.productName(),
-    date: faker.date.anytime().toISOString(),
-    type: 'EXPENSE',
-    amount: Number(faker.finance.amount()),
-  };
-
   class UpdateTransactionRepositoryStub {
-    async execute(transactionId) {
-      return {
-        id: transactionId,
-        ...transaction,
-      };
+    async execute() {
+      return transaction;
     }
   }
   const makeSut = () => {
