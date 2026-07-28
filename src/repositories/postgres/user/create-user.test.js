8c97538faa0e063d64a762fdae5574620599b3aa
Danner Kurtz <kurtz.danner@gmail.com>
2026-07-28 06:11:55 -0300

    test: adicionar teste para criação de usuário no repositório Postgres

---
+++ src/repositories/postgres/user/create-user.test.js
@@ -0,0 +1,12 @@
+import { user } from '../../../tests';
+import { PostgresCreateUserRepository } from './create-user';
+
+describe('CreateUserRepository', () => {
+  it('should create a user on db', async () => {
+    const sut = new PostgresCreateUserRepository();
+
+    const result = await sut.execute(user);
+
+    expect(result).toBeTruthy();
+  });
+});
