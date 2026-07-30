109c4e5d839b0d2090f4faaa3745da11328a6a10
Danner Kurtz <kurtz.danner@gmail.com>
2026-07-30 19:08:11 -0300

    test: adicionar teste para verificar a criação de uma transação

---
+++ src/repositories/postgres/transaction/create-transaction.test.js
@@ -0,0 +1,21 @@
+import dayjs from 'dayjs';
+import { prisma } from '../../../../prisma/prisma';
+import { user as fakerUser, transaction } from '../../../tests';
+import { PostgresCreateTransactionRepository } from './create-transaction';
+
+describe('PostgresCreateTransactionRepository', () => {
+  it('should create a transaction', async () => {
+    const user = await prisma.user.create({ data: fakerUser });
+    const sut = new PostgresCreateTransactionRepository();
+
+    const result = await sut.execute({ ...transaction, userId: user.id });
+
+    expect(result.name).toBe(transaction.name);
+    expect(result.amount.toString()).toBe(transaction.amount.toString());
+    expect(result.type).toBe(transaction.type);
+    expect(result.user_id).toBe(user.id);
+    expect(dayjs(result.date).daysInMonth()).toBe(
+      dayjs(transaction.date).daysInMonth(),
+    );
+  });
+});
