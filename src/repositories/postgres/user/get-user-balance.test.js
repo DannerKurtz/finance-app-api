b7cdd6ab5eead623a074e62dbb5ba000f48f90e0
Danner Kurtz <kurtz.danner@gmail.com>
2026-07-30 06:31:52 -0300

    test: adicionar teste para verificar se uma exceção é lançada quando o Prisma falha

--- src/repositories/postgres/user/get-user-balance.test.js
+++ src/repositories/postgres/user/get-user-balance.test.js
@@ -97,4 +97,15 @@ describe('PostgresGetUserBalanceRepository', () => {
       },
     });
   });
+
+  it('should throw if Prisma throws', async () => {
+    const sut = new PostgresGetUserBalanceRepository();
+    jest
+      .spyOn(prisma.transaction, 'aggregate')
+      .mockRejectedValueOnce(new Error());
+
+    const promise = sut.execute(fakerUser.id);
+
+    await expect(promise).rejects.toThrow();
+  });
 });
