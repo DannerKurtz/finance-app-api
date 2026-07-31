8dcbea0b3759ec98b4a64a7eac919508b87d99fd
Danner Kurtz <kurtz.danner@gmail.com>
2026-07-31 20:33:03 -0300

    test: adicionar teste para verificar se o Prisma é chamado com os valores corretos ao excluir uma transação

--- src/repositories/postgres/transaction/delete-transaction.test.js
+++ src/repositories/postgres/transaction/delete-transaction.test.js
@@ -1,3 +1,4 @@
+import { jest } from '@jest/globals';
 import { prisma } from '../../../../prisma/prisma';
 import { user as fakerUser, transaction } from '../../../tests';
 import { PostgresDeleteTransactionRepository } from './delete-transaction';
@@ -16,5 +17,20 @@ describe('PostgresDeleteTransactionRepository', () => {
     const result = await sut.execute(transactionToDelete.id);
 
     expect(result.id).toBe(transactionToDelete.id);
+  });
+
+  it('should call Prisma with correct values', async () => {
+    const prismaSpy = jest
+      .spyOn(prisma.transaction, 'delete')
+      .mockResolvedValue(transaction);
+    const sut = new PostgresDeleteTransactionRepository();
+
+    await sut.execute(transaction.id);
+
+    expect(prismaSpy).toHaveBeenCalledWith({
+      where: {
+        id: transaction.id,
+      },
+    });
   });
 });
