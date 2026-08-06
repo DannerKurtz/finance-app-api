3a8dcd6fccdd4a28f4ba700b513a497c8daf4b59
Danner Kurtz <kurtz.danner@gmail.com>
2026-08-05 22:58:24 -0300

    test: adicionar teste para verificar a funcionalidade do PasswordHasherAdapter

---
+++ src/adapters/password-hasher.test.js
@@ -0,0 +1,11 @@
+import { PasswordHasherAdapter } from './password-hasher';
+
+describe('PasswordHasherAdapter', () => {
+  it('should hash a password', async () => {
+    const sut = new PasswordHasherAdapter();
+    const password = 'myPassword123';
+    const hashedPassword = await sut.execute(password);
+    expect(hashedPassword).toBeDefined();
+    expect(hashedPassword).not.toEqual(password);
+  });
+});
