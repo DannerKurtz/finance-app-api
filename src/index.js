130f61bcbf6c6420705e28a246524c97b7cdc876
Danner Kurtz <kurtz.danner@gmail.com>
2026-08-06 20:05:39 -0300

    refactor: refatorar rotas de usuários para o novo arquivo users.js

--- src/index.js
+++ src/index.js
@@ -1,77 +1,59 @@
 import 'dotenv/config.js';
 import express from 'express';
-import { makeCreateTransactionController, makeDeleteTransactionController, makeGetTransactionsByUserIdController, makeUpdateTransactionController } from './factory/controller/transaction.js';
-import { makeCreateUserController, makeDeleteUserController, makeGetUserBalanceController, makeGetUserByIdController, makeUpdateUserController } from './factory/controller/user.js';
+import {
+  makeCreateTransactionController,
+  makeDeleteTransactionController,
+  makeGetTransactionsByUserIdController,
+  makeUpdateTransactionController,
+} from './factory/controller/transaction.js';
+import { usersRouter } from './routes/users.js';
 
-const app = express() 
-app.use(express.json())
+const app = express();
+app.use(express.json());
 
-app.post('/api/users', async (req, res) => {
-
-  const createUserController = makeCreateUserController()
-  const createUserResponse = await createUserController.execute(req);
-
-  res.status(createUserResponse.statusCode).json(createUserResponse.body);
-})
-
-app.patch('/api/users/:userId', async (req, res) => {
-  const updateUserController = makeUpdateUserController()
-  const updateUserRequest = await updateUserController.execute(req);
-  res.status(updateUserRequest.statusCode).json(updateUserRequest.body);
-})
-
-app.get('/api/users/:userId', async (req, res) => {
-  const getUserByIdController = makeGetUserByIdController();
-
-  const getUserByIdResponse = await getUserByIdController.execute(req);
-
-  res.status(getUserByIdResponse.statusCode).json(getUserByIdResponse.body);
-});
-  
-app.get('/api/users/:userId/balance', async (req, res) => {
-  const getUserBalanceController = makeGetUserBalanceController();
-
-  const getUserBalanceResponse = await getUserBalanceController.execute(req);
-
-  res.status(getUserBalanceResponse.statusCode).json(getUserBalanceResponse.body);
-})
-
-app.delete('/api/users/:userId', async (req, res) => {
-  const deleteUserController = makeDeleteUserController()
-
-  const deleteUserResponse = await deleteUserController.execute(req);
-  
-  res.status(deleteUserResponse.statusCode).json(deleteUserResponse.body);
-})
+app.use('/api/users', usersRouter);
 
 app.get('/api/transactions', async (req, res) => {
-  const getTransactionsByUserIdController = makeGetTransactionsByUserIdController();
-  const getTransactionsByUserIdResponse = await getTransactionsByUserIdController.execute(req);
+  const getTransactionsByUserIdController =
+    makeGetTransactionsByUserIdController();
+  const getTransactionsByUserIdResponse =
+    await getTransactionsByUserIdController.execute(req);
 
-  res.status(getTransactionsByUserIdResponse.statusCode).json(getTransactionsByUserIdResponse.body);
-})
+  res
+    .status(getTransactionsByUserIdResponse.statusCode)
+    .json(getTransactionsByUserIdResponse.body);
+});
 
 app.post('/api/transactions', async (req, res) => {
   const createTransactionController = makeCreateTransactionController();
-  const createTransactionResponse = await createTransactionController.execute(req);
+  const createTransactionResponse =
+    await createTransactionController.execute(req);
 
-  res.status(createTransactionResponse.statusCode).json(createTransactionResponse.body);
-})
+  res
+    .status(createTransactionResponse.statusCode)
+    .json(createTransactionResponse.body);
+});
 
 app.patch('/api/transactions/:transactionId', async (req, res) => {
   const updateTransactionController = makeUpdateTransactionController();
-  const updateTransactionResponse = await updateTransactionController.execute(req);
+  const updateTransactionResponse =
+    await updateTransactionController.execute(req);
 
-  res.status(updateTransactionResponse.statusCode).json(updateTransactionResponse.body);
-})
+  res
+    .status(updateTransactionResponse.statusCode)
+    .json(updateTransactionResponse.body);
+});
 
 app.delete('/api/transactions/:transactionId', async (req, res) => {
   const deleteTransactionController = makeDeleteTransactionController();
-  const deleteTransactionResponse = await deleteTransactionController.execute(req);
+  const deleteTransactionResponse =
+    await deleteTransactionController.execute(req);
 
-  res.status(deleteTransactionResponse.statusCode).json(deleteTransactionResponse.body);
-})
+  res
+    .status(deleteTransactionResponse.statusCode)
+    .json(deleteTransactionResponse.body);
+});
 
 app.listen(process.env.PORT, () => {
   console.log(`Server is running on http://localhost:${process.env.PORT}`);
-})
+});
---
+++ src/routes/users.js
@@ -0,0 +1,48 @@
+import { Router } from 'express';
+import {
+  makeCreateUserController,
+  makeDeleteUserController,
+  makeGetUserBalanceController,
+  makeGetUserByIdController,
+  makeUpdateUserController,
+} from '../factory/controller/user';
+
+export const usersRouter = Router();
+usersRouter.post('/', async (req, res) => {
+  const createUserController = makeCreateUserController();
+  const createUserResponse = await createUserController.execute(req);
+
+  res.status(createUserResponse.statusCode).json(createUserResponse.body);
+});
+
+usersRouter.patch('/:userId', async (req, res) => {
+  const updateUserController = makeUpdateUserController();
+  const updateUserRequest = await updateUserController.execute(req);
+  res.status(updateUserRequest.statusCode).json(updateUserRequest.body);
+});
+
+usersRouter.get('/:userId', async (req, res) => {
+  const getUserByIdController = makeGetUserByIdController();
+
+  const getUserByIdResponse = await getUserByIdController.execute(req);
+
+  res.status(getUserByIdResponse.statusCode).json(getUserByIdResponse.body);
+});
+
+usersRouter.get('/:userId/balance', async (req, res) => {
+  const getUserBalanceController = makeGetUserBalanceController();
+
+  const getUserBalanceResponse = await getUserBalanceController.execute(req);
+
+  res
+    .status(getUserBalanceResponse.statusCode)
+    .json(getUserBalanceResponse.body);
+});
+
+usersRouter.delete('/:userId', async (req, res) => {
+  const deleteUserController = makeDeleteUserController();
+
+  const deleteUserResponse = await deleteUserController.execute(req);
+
+  res.status(deleteUserResponse.statusCode).json(deleteUserResponse.body);
+});
