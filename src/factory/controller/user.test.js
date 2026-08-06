e8361c89c1547d14b68715dd4cac6de5ae6098ad
Danner Kurtz <kurtz.danner@gmail.com>
2026-08-05 22:48:29 -0300

    test: adicionar teste para verificar a criação de instância do UpdateUserController

--- src/factory/controller/user.test.js
+++ src/factory/controller/user.test.js
@@ -1,8 +1,12 @@
-import { CreateUserController } from '../../controllers';
-import { makeCreateUserController } from './user';
+import { CreateUserController, UpdateUserController } from '../../controllers';
+import { makeCreateUserController, makeUpdateUserController } from './user';
 
 describe('User Controller Factory', () => {
   it('should return a valid CreateUserController instance', () => {
     expect(makeCreateUserController()).toBeInstanceOf(CreateUserController);
+  });
+
+  it('should return a valid UpdateUserController instance', () => {
+    expect(makeUpdateUserController()).toBeInstanceOf(UpdateUserController);
   });
 });
