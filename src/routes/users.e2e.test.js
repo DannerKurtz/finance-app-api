0755123881747d62a54a59ef4621a99e9cb68df2
Danner Kurtz <kurtz.danner@gmail.com>
2026-08-08 20:29:23 -0300

    feat: adicionar teste E2E para obter saldo do usuário após transação

--- src/routes/users.e2e.test.js
+++ src/routes/users.e2e.test.js
@@ -1,7 +1,7 @@
 import { faker } from '@faker-js/faker';
 import request from 'supertest';
 import { app } from '../app.js';
-import { user } from './../tests/index.js';
+import { transaction, user } from './../tests/index.js';
 describe('User Routes E2E Tests', () => {
   it('POST /users should return 201 when user is created', async () => {
     const response = await request(app)
@@ -55,6 +55,31 @@ describe('User Routes E2E Tests', () => {
       });
     const response = await request(app).delete(
       `/api/users/${createdUser.body.createdUser.id}`,
+    );
+
+    expect(response.status).toBe(200);
+  });
+
+  it('GET /api/:userId/balance should return 200 when return balance', async () => {
+    const { id, ...fakerUser } = user;
+    const createdUser = await request(app)
+      .post('/api/users')
+      .send({
+        ...fakerUser,
+      });
+
+    const transactionC = await request(app)
+      .post(`/api/transactions`)
+      .send({
+        ...transaction,
+        amount: 1000,
+        type: 'INVESTMENT',
+        user_id: createdUser.body.createdUser.id,
+      });
+
+    console.log('Transaction created:', transactionC.body);
+    const response = await request(app).get(
+      `/api/users/${createdUser.body.createdUser.id}/balance`,
     );
 
     expect(response.status).toBe(200);
