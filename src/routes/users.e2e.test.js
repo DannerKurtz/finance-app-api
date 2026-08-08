e4753161b3d9a99d92d96a67c949efb4bbb4a99b
Danner Kurtz <kurtz.danner@gmail.com>
2026-08-08 19:27:02 -0300

    test: Realizado o teste do Get Users E2E

--- src/routes/users.e2e.test.js
+++ src/routes/users.e2e.test.js
@@ -1,5 +1,5 @@
 import request from 'supertest';
-import { app } from '../index.js';
+import { app } from '../app.js';
 import { user } from './../tests/index.js';
 describe('User Routes E2E Tests', () => {
   it('POST /users should return 201 when user is created', async () => {
@@ -11,5 +11,19 @@ describe('User Routes E2E Tests', () => {
       });
 
     expect(response.status).toBe(201);
+  });
+
+  it('GET /api/:userId should return 200 when user is found', async () => {
+    const createdUser = await request(app)
+      .post('/api/users')
+      .send({
+        ...user,
+        id: undefined,
+      });
+    const response = await request(app).get(
+      `/api/users/${createdUser.body.createdUser.id}`,
+    );
+
+    expect(response.status).toBe(200);
   });
 });
