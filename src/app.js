d542653f2748ee92e99d325ab443d95aebc2939f
Danner Kurtz <kurtz.danner@gmail.com>
2026-08-17 22:23:53 -0300

    feat: adicionar documentação Swagger à API

--- src/app.js
+++ src/app.js
@@ -1,9 +1,21 @@
 import express from 'express';
+import fs from 'fs';
+import path from 'path';
+import swaggerUi from 'swagger-ui-express';
 import { transactionRouter } from './routes/transactions.js';
 import { usersRouter } from './routes/users.js';
-
 export const app = express();
+
 app.use(express.json());
 
 app.use('/api/users', usersRouter);
 app.use('/api/transactions', transactionRouter);
+
+const swaggerDocument = JSON.parse(
+  fs.readFileSync(
+    path.join(import.meta.dirname, '../docs/swagger.json'),
+    'utf8',
+  ),
+);
+
+app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
