0cda5da9391b4b7290ae13365cafadd37dfaede9
Danner Kurtz <kurtz.danner@gmail.com>
2026-09-13 22:04:37 -0300

    fix: adicionar configuração do Prisma e adaptar conexão com PostgreSQL

--- prisma/prisma.js
+++ prisma/prisma.js
@@ -1,2 +1,8 @@
+import { PrismaPg } from '@prisma/adapter-pg';
+import 'dotenv/config';
 import { PrismaClient } from '../src/generated/prisma/index.js';
-export const prisma = new PrismaClient();
+
+const adapter = new PrismaPg({
+  connectionString: process.env.DATABASE_URL,
+});
+export const prisma = new PrismaClient({ adapter });
