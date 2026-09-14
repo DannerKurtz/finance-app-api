f63bc610da0e0b27b0425533119d5b20a14c7bc2
Danner Kurtz <kurtz.danner@gmail.com>
2026-09-13 22:26:48 -0300

    fix: adicionar comando para gerar prisma no setup do Jest

--- jest.global-setup.mjs
+++ jest.global-setup.mjs
@@ -1,6 +1,7 @@
 import { execSync } from 'node:child_process';
 
 async function init() {
+  execSync('npx prisma generate', { stdio: 'inherit' });
   execSync('docker compose up -d --wait postgres-test');
   execSync('npx prisma db push');
 }
